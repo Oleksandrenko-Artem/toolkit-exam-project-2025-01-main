8ac1441ecfb86f005cbda338176469906fdab9cc
Artem Oleksandrenko <artem2008.zp@gmail.com>
2025-11-30 22:24:14 +0200

    Add FirstNamingPlatformList

---
+++ client/src/pages/Home/FirstNamingPlatformList/firstNamingPlatform.js
@@ -0,0 +1,28 @@
+import CONSTANTS from "./../../../constants";
+
+const { STATIC_IMAGES_PATH } = CONSTANTS;
+
+const firstNamingPlatformCards = [
+    {
+        "iconSrc": `${STATIC_IMAGES_PATH}first_naming_platform/star.svg`,
+        "title": "50k+ Customers",
+        "body": "Rated Excellent on Trustpilot",
+    },
+    {
+        "iconSrc": `${STATIC_IMAGES_PATH}first_naming_platform/growing.svg`,
+        "title": "4 Times Inc. 5000",
+        "body": "Fastest-Growing Companies in the US",
+    },
+    {
+        "iconSrc": `${STATIC_IMAGES_PATH}first_naming_platform/award-solid.svg`,
+        "title": "2025 Trailblazer Award",
+        "body": "Honored by the Internet Commerce Association",
+    },
+    {
+        "iconSrc": `${STATIC_IMAGES_PATH}first_naming_platform/bulb.svg`,
+        "title": "Most Innovative",
+        "body": "Recognized by Inc. Magazine",
+    },
+];
+
+export default firstNamingPlatformCards;
